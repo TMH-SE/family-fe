54ab765e86f29b9cefca6c21bf98ec7b12d70727
Tran Nhu <hotrannhu@gmail.com>
2020-06-21 22:12:54 +0700

    highlight group in mobile

--- src/components/community/index.js
+++ src/components/community/index.js
@@ -8,7 +8,13 @@ import JoinBtn from '../joinBtn'
 
 function CommunityItem(props) {
   const { item } = props
-  const { refetchCount, setRefetchCount, refetchSumPosts, setRefetchSumPosts, me } = useContext(IContext)
+  const {
+    refetchCount,
+    setRefetchCount,
+    refetchSumPosts,
+    setRefetchSumPosts,
+    me
+  } = useContext(IContext)
   const history = useHistory()
   // const [ dataMems, setDataMems ] = useState(null)
   const { data, refetch, loading } = useQuery(GET_POST_BY_COMMUNITY, {
@@ -16,7 +22,11 @@ function CommunityItem(props) {
     fetchPolicy: 'no-cache',
     skip: !item?._id
   })
-  const { data: dataMemberCount, refetch: refetchDataMemberCount, loading: loadingSumMem } = useQuery(GET_MEMBERS_BY_COMMUNITY, {
+  const {
+    data: dataMemberCount,
+    refetch: refetchDataMemberCount,
+    loading: loadingSumMem
+  } = useQuery(GET_MEMBERS_BY_COMMUNITY, {
     variables: { communityId: item?._id },
     fetchPolicy: 'no-cache',
     skip: !item?._id
@@ -27,10 +37,14 @@ function CommunityItem(props) {
     setRefetchCount('')
     setRefetchSumPosts('')
   }, [refetchCount, refetchSumPosts])
-  return (
-    (loading || loadingSumMem) ? <Skeleton active></Skeleton> :
+  return loading || loadingSumMem ? (
+    <Skeleton active></Skeleton>
+  ) : (
     <List.Item
-      onClick={() => history.push(`/pagegroup/${item?._id}`)}
+      onClick={() => {
+        props?.setShowCommunities && props.setShowCommunities(false)
+        history.push(`/pagegroup/${item?._id}`)
+      }}
       style={{ backgroundColor: '#fff', marginBottom: 10, padding: 10 }}
     >
       <List.Item.Meta
@@ -46,10 +60,12 @@ function CommunityItem(props) {
         }
         description={`${dataMemberCount?.getMembersByCommunity} thành viên - ${data?.postsByCommunity?.length} bài viết`}
       />
-       {props.isActionJoin && <JoinBtn id={{ userId: me?._id, communityId: item?._id }}/>}
+      {props.isActionJoin && (
+        <JoinBtn id={{ userId: me?._id, communityId: item?._id }} />
+      )}
     </List.Item>
-    // )}
-    // />
   )
+  // )}
+  // />
 }
 export default CommunityItem
