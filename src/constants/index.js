5e9a4fa6ddda104c1263b2edacc6ae030bc838a8
TMH-SE <hieuhutieu98@gmail.com>
2020-06-22 18:20:15 +0700

    test

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,7 +1,8 @@
 export const configRTCPeerConnection = {
   iceServers: [
-    { urls: 'stun:stun.services.mozilla.com' },
-    { urls: 'stun:stun.l.google.com:19302' },
+    {
+      urls: ['stun:stun.l.google.com:19302', 'stun:stun.services.mozilla.com']
+    },
     {
       urls: process.env.TURN_SERVER,
       credential: process.env.TURN_PASSWORD,
