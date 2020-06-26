df05d44c44d025846d356c61122d8a1725bf6af9
Tran Nhu <hotrannhu@gmail.com>
2020-06-26 12:30:58 +0700

    loading post

--- src/pages/homepage/index.js
+++ src/pages/homepage/index.js
@@ -56,7 +56,7 @@ const HomePage = props => {
 
   useEffect(() => {
     setDataPostLoad(dataPosts)
-    if (dataPosts?.posts.length < 5) {
+    if (dataPosts?.posts?.length < 5) {
       setIsEnd(true)
     }
   }, [dataPosts])
