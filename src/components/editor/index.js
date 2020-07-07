03ce45c2bea7b39305c9c4f34955b229fb6ee7ec
TMH-SE <hieuhutieu98@gmail.com>
2020-07-08 01:21:43 +0700

    fix editor mobile

--- src/components/editor/index.js
+++ src/components/editor/index.js
@@ -1,15 +1,43 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import CKEditor from '@ckeditor/ckeditor5-react'
 import FullEditor from 'ckeditor5-build-full'
 import { CustomUploadAdapterPlugin } from './customAdapter'
+import { Grid } from 'antd'
 
 const index = ({ setEditor, initialValue }) => {
+  const mobileToolbar = [
+    'heading',
+    '|',
+    'bold',
+    'italic',
+    'underline',
+    '|',
+    'bulletedList',
+    'numberedList',
+    '|',
+    'alignment',
+    '|',
+    'link',
+    'imageUpload',
+    'mediaEmbed'
+  ]
+  const screens = Grid.useBreakpoint()
+  const isBreak = useMemo(
+    () =>
+      Object.entries(screens).filter(v => !v[1] && v[0] === 'md').length === 1,
+    [screens]
+  )
+  if (Object.keys(screens).length === 0) {
+    return null
+  }
+
   return (
     <CKEditor
       editor={FullEditor}
       onInit={editor => setEditor(editor)}
       data={initialValue || ''}
       config={{
+        ...(isBreak ? { toolbar: mobileToolbar } : {}),
         extraPlugins: [CustomUploadAdapterPlugin]
       }}
     />
